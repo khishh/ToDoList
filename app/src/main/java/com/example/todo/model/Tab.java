f178b6bb8a4b3422dff9869da8913a121bc9183f
khishh <khiraishi0319@gmail.com>
2020-07-08 19:40:20 +0900

    Added Tab class in model

---
+++ app/src/main/java/com/example/todo/model/Tab.java
@@ -0,0 +1,57 @@
+package com.example.todo.model;
+
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Tab {
+
+    @PrimaryKey (autoGenerate = true)
+    private int tabId;
+
+    private int tabIndex;
+
+    private String tabTitle;
+
+    @Ignore
+    private List<ToDo> toDoList = new ArrayList<>();
+
+    //  getter and setter
+
+    public int getTabId() {
+        return tabId;
+    }
+
+    public void setTabId(int tabIndex) {
+        this.tabIndex = tabIndex;
+    }
+
+    public int getTabIndex() {
+        return tabIndex;
+    }
+
+    public void setTabIndex(int tabIndex) {
+        this.tabIndex = tabIndex;
+    }
+
+    public String getTabTitle() {
+        return tabTitle;
+    }
+
+    public void setTabTitle(String tabTitle) {
+        this.tabTitle = tabTitle;
+    }
+
+    public List<ToDo> getToDoList() {
+        return toDoList;
+    }
+
+    public void setToDoList(List<ToDo> toDoList) {
+        this.toDoList = toDoList;
+    }
+}
