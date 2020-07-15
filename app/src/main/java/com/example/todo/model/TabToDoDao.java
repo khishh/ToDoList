cf1ed0edd5ae36b3f52a47a59bfce9548d388eed
khishh <khiraishi0319@gmail.com>
2020-07-15 12:44:40 +0900

    Changed @Insert Dao method from void to returning Long. This return number will be assigned for Tab/ToDo id field.

--- app/src/main/java/com/example/todo/model/TabToDoDao.java
+++ app/src/main/java/com/example/todo/model/TabToDoDao.java
@@ -16,40 +16,57 @@ public abstract class TabToDoDao {
     private static final String TAG = "TabToDoDao";
 
     @Insert
-    public abstract void insertTab(Tab tab);
+    public abstract Long insertTab(Tab tab);
 
     @Insert
-    public abstract void insertToDoList(List<ToDo> toDoList);
+    public abstract List<Long> insertToDoList(List<ToDo> toDoList);
+
+    @Delete
+    public abstract void deleteTab(List<Tab> tabs);
 
     @Update
     public abstract void updateTab(Tab tab);
 
     @Update
-    public abstract  void updateToDoList(List<ToDo> toDos);
+    public abstract void updateToDoList(List<ToDo> toDos);
 
     @Query("SELECT * FROM Tab")
     public abstract List<Tab> getAllTab();
 
-    @Query("DELETE FROM ToDo where toDoOwnerIndex = :toDoOwnerIndex")
-    public abstract void deleteAllToDoOfIndex(int toDoOwnerIndex);
+    @Query("DELETE FROM Tab where tabIndex = :tabIndex")
+    public abstract  void deleteTabOfIndex(int tabIndex);
+
+    @Query("DELETE FROM ToDo where toDoOwnerId = :toDoOwnerId")
+    public abstract void deleteAllToDoOfId(int toDoOwnerId);
 
     @Query("SELECT * FROM Tab where tabIndex = :tabIndex")
     public abstract Tab getTab(int tabIndex);
 
-    @Query("SELECT * FROM ToDo where toDoOwnerIndex = :toDoOwnerIndex")
-    public abstract List<ToDo> getToDoList(int toDoOwnerIndex);
+    @Query("SELECT * FROM ToDo where toDoOwnerId = :toDoOwnerId")
+    public abstract List<ToDo> getToDoList(int toDoOwnerId);
 
     public void insertToDoWithTab(Tab tab){
+
+        int tabId = insertTab(tab).intValue();
+        tab.setTabId(tabId);
+
+
         List<ToDo> toDoList = tab.getToDoList();
 
+        Log.d(TAG, "tab's index == " + tab.getTabIndex() + " tabId == "+tab.getTabId());
         for(int i = 0; i < toDoList.size(); i++){
-            toDoList.get(i).setToDoOwnerIndex(tab.getTabIndex());
-            Log.d(TAG, tab.getTabIndex() + " " + toDoList.get(i).getToDoOwnerIndex());
+            toDoList.get(i).setToDoOwnerId(tab.getTabId());
+            Log.d(TAG, "i = " + i + " " + tab.getTabId() + " " + toDoList.get(i).getToDoOwnerId());
         }
 
         Log.d(TAG, "Size of toDoList == " + toDoList.size());
-        insertToDoList(toDoList);
-        insertTab(tab);
+
+        List<Long> toDoIds = insertToDoList(toDoList);
+
+        for(int i = 0; i < toDoList.size(); i++){
+            toDoList.get(i).setToDoId(toDoIds.get(i).intValue());
+            Log.d(TAG, "toDoId at " + i + " " + toDoIds.get(i).intValue());
+        }
     }
 
     public Tab getTabWithToDo(int tabIndex){
