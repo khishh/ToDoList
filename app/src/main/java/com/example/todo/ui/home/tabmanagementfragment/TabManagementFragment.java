62a25102c4424fdd7f345134cf667201998fc8f5
khishh <khiraishi0319@gmail.com>
2020-07-17 00:10:23 +0900

    implemented add new tabs and swap/delete existing tabs

--- app/src/main/java/com/example/todo/ui/home/tabmanagementfragment/TabManagementFragment.java
+++ app/src/main/java/com/example/todo/ui/home/tabmanagementfragment/TabManagementFragment.java
@@ -1,24 +1,34 @@
 package com.example.todo.ui.home.tabmanagementfragment;
 
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.todo.R;
 import com.example.todo.model.Tab;
@@ -40,6 +50,12 @@ public class TabManagementFragment extends Fragment {
 
     private LinearLayoutManager linearLayoutManager;
 
+    private ImageView tabCloseBtn;
+
+    private ImageButton addTabBtn;
+
+    private EditText userInputTab;
+
     // ui components
     private RecyclerView recyclerView;
 
@@ -58,7 +74,9 @@ public class TabManagementFragment extends Fragment {
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
             final int fromPos = viewHolder.getAbsoluteAdapterPosition();
             final int toPos = target.getAbsoluteAdapterPosition();
-            adapter.swapTabList(fromPos, toPos);
+
+//            adapter.swapTabList(fromPos, toPos);
+
             adapter.notifyItemMoved(fromPos, toPos);
 
             Log.d(TAG, fromPos + " " + toPos);
@@ -76,9 +94,7 @@ public class TabManagementFragment extends Fragment {
 
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-//            if(direction == ItemTouchHelper.LEFT){
-//                adapter.notifyItemChanged(viewHolder.getAbsoluteAdapterPosition());
-//            }
+
         }
 
         @Override
@@ -101,48 +117,6 @@ public class TabManagementFragment extends Fragment {
         public boolean isLongPressDragEnabled() {
             return false;
         }
-
-        @Override
-        public int convertToAbsoluteDirection(int flags, int layoutDirection) {
-            return super.convertToAbsoluteDirection(flags, layoutDirection);
-        }
-
-//        @Override
-//        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-//            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-//
-//            View itemView = viewHolder.itemView;
-//            int backgroundCornerOffset = 20;
-//
-//            int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
-//            int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
-//            int iconBottom = iconTop + icon.getIntrinsicHeight();
-//
-//            if(dX < 0){
-//
-//                itemView.setTranslationX(dX/7);
-//                int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
-//                int iconRight = itemView.getRight() - iconMargin;
-//                icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
-//
-//                Log.d(TAG, "dx == " + dX);
-//
-//                if(dX <= -100f){
-//                    dX = -100f;
-//                }
-//
-//
-//                background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
-//                        itemView.getTop(), itemView.getRight(), itemView.getBottom());
-//            }
-//            else{
-//                background.setBounds(0,0,0,0);
-//            }
-//            background.draw(c);
-//            icon.draw(c);
-//        }
-
-
     }
 
 
@@ -156,6 +130,14 @@ public class TabManagementFragment extends Fragment {
         if (getArguments() != null) {
 
         }
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                Navigation.findNavController(tabCloseBtn).navigate(R.id.action_tabManagementFragment_to_navigation_home);
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
     }
 
     @Override
@@ -167,6 +149,7 @@ public class TabManagementFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+
 
         viewModel = new ViewModelProvider(this).get(TabManagementViewModel.class);
         viewModel.retrieveTabList();
@@ -192,12 +175,46 @@ public class TabManagementFragment extends Fragment {
             public void onSwipeDeleteBack(int position) {
                 Log.d(TAG, "onSwipeDeleteBack pos = " + position);
                 TabManagementAdapter.ViewHolder viewHolder = adapter.getViewByPosition(position);
-//                float x = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 0, recyclerView.getResources().getDisplayMetrics());
                 viewHolder.linearLayout.animate().translationX(0).setDuration(300).start();
+            }
+
+            @Override
+            public void deleteTabAtPosition(int position) {
+                final int deletePos = position;
+                adapter.notifyItemRemoved(deletePos);
+                viewModel.deleteTab(deletePos);
             }
         });
 
 
+        tabCloseBtn = view.findViewById(R.id.tab_manage_close);
+        tabCloseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(tabCloseBtn).navigate(R.id.action_tabManagementFragment_to_navigation_home);
+            }
+        });
+
+        userInputTab = view.findViewById(R.id.tab_management_edit_text);
+        addTabBtn = view.findViewById(R.id.tab_management_add_tab);
+        addTabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newTabTitle = userInputTab.getText().toString();
+
+                if(!TextUtils.isEmpty(newTabTitle)){
+                    viewModel.addNewTab(newTabTitle);
+                    userInputTab.setText("");
+                }
+                else{
+                    Toast.makeText(getContext(), "Tab's title is empty", Toast.LENGTH_SHORT).show();
+                }
+
+                userInputTab.clearFocus();
+                hideKeyboard(userInputTab);
+            }
+        });
+        
         callBack = new TabManagementItemCallBack(
                 ItemTouchHelper.DOWN | ItemTouchHelper.UP,
                 0);
@@ -218,5 +235,13 @@ public class TabManagementFragment extends Fragment {
 
     private void startDragging(TabManagementAdapter.ViewHolder viewHolder){
         helper.startDrag(viewHolder);
+    }
+
+
+    private void hideKeyboard(View view){
+        InputMethodManager inputMethodManager = (InputMethodManager)view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert inputMethodManager != null;
+
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
