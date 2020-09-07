4149b00ed596574d9aa59ece61c69b1c07d15bca
khishh <khiraishi0319@gmail.com>
2020-09-07 12:18:07 +0900

    added class level explanation in MainActivity and clean its xml file

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -13,28 +13,30 @@ import androidx.fragment.app.FragmentTransaction;
 /**
  *  MainActivity
  *  Simple activity holding a FrameLayout
- *  Only Managing fragment translation in this activity
+ *  Only Managing fragment translation in this activity.
+ *  Initial fragment shown is HomeFragment.
+ *
+ *  Possible Fragments in FrameLayout : {HomeFragment, ItemManagementFragment, TabManagementFragment}
  */
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private static final String KEY_HOME_FRAGMENT = "home_fragment";
-    private static final String KEY_TAB_MANAGEMENT = "tab_management";
+    private static final String TAG_HOME_FRAGMENT = HomeFragment.class.getSimpleName();
+    private static final String TAG_ITEM_MANAGEMENT_FRAGMENT = ItemManagementFragment.class.getSimpleName();
+    private static final String TAG_TAB_MANAGEMENT_FRAGMENT = TabManagementFragment.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.d(TAG, "MainActivity created");
-
         createHomeFragment();
     }
 
     /**
-     * show HomeFragment (default fragment)
+     * show HomeFragment (the launching fragment)
      * only called when the app starts
      */
     public void createHomeFragment(){
@@ -44,78 +46,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * show TabManagementFragment
+     * Display TabManagementFragment
+     * A fragment where users can add/delete/sort/edit Tab
      */
     public void createTabManagementFragment(){
         TabManagementFragment fragment = TabManagementFragment.getInstance();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//        ft.setCustomAnimations(R.anim.slide_from_down, R.anim.slide_to_up).replace(R.id.container, fragment).commit();
         ft.setCustomAnimations(R.anim.slide_from_down, R.anim.slide_to_up, R.anim.slide_from_top, R.anim.slide_to_down)
                 .add(R.id.container, fragment)
-                .addToBackStack(TabManagementFragment.class.getSimpleName())
+                .addToBackStack(TAG_TAB_MANAGEMENT_FRAGMENT)
                 .commit();
     }
 
+    /**
+     * Display ItemManagementFragment
+     * A fragment where users can add/delete/sort To-Do items
+     */
     public void createItemManagementFragment(int tabId){
         ItemManagementFragment fragment = ItemManagementFragment.newInstance(tabId);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.setCustomAnimations(R.anim.slide_from_left, R.anim.slide_to_right, R.anim.slide_from_right, R.anim.slide_to_left)
                 .add(R.id.container, fragment)
-                .addToBackStack(ItemManagementFragment.class.getSimpleName())
+                .addToBackStack(TAG_ITEM_MANAGEMENT_FRAGMENT)
                 .commit();
     }
 
+    /**
+     * Pop the fragment on top of the BackStack
+     * -> take users back to the previous screen
+     */
     public void popOffFragment(){
         getSupportFragmentManager().popBackStack();
     }
 
+    /**
+     * After popping off the fragment on top of the BackStack,
+     * replace a new HomeFragment with the old HomeFragment
+     * only called when Tab was deleted in the TabManagementFragment as a pager needs to be refreshed
+     * after removing one of its Tabs
+     */
     public void popOffFragmentAndResetHomeFragment(){
-        getSupportFragmentManager().popBackStack();
+        popOffFragment();
         HomeFragment fragment = HomeFragment.getInstance();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.container, fragment).commit();
     }
-
-    /**
-     * show HomeFragment called from other fragments to come back
-     * using replace to show the up-to-date data
-     */
-    public void updateHomeFragmentFromTabManagement(){
-        Log.d(TAG, "updateHomeFragment");
-        HomeFragment fragment = HomeFragment.getInstance();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.setCustomAnimations(R.anim.slide_from_top, R.anim.slide_to_down).replace(R.id.container, fragment).commit();
-    }
-
-    public void updateHomeFragmentFromItemManagement(){
-        Log.d(TAG, "updateHomeFragment");
-        HomeFragment fragment = HomeFragment.getInstance();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.setCustomAnimations(R.anim.slide_from_right, R.anim.slide_to_left).replace(R.id.container, fragment).commit();
-    }
-
-    // for later use
-//    //    detect if screen shows keyboard
-//    //    https://stackoverflow.com/questions/2150078/how-to-check-visibility-of-software-keyboard-in-android
-//    private void setKeyBoardOpenListener(){
-//        rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-//            @Override
-//            public void onGlobalLayout() {
-//                Log.d(TAG, String.valueOf(rootView.getRootView().getHeight()) + " " + rootView.getHeight());
-//                int heightDiff = rootView.getRootView().getHeight() - rootView.getHeight();
-//                if(heightDiff > Util.dpToPx(rootView.getContext(), 200)){
-//                    navView.setVisibility(View.GONE);
-//                }
-//                else{
-//                    new Handler().postDelayed(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            navView.setVisibility(View.VISIBLE);
-//                        }
-//                    }, 50);
-//
-//                }
-//            }
-//        });
-//    }
 }
